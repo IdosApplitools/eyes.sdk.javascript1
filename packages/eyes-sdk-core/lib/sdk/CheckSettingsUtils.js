cd1a0637533940c556cb62a7470e03ed024e4545
Dave Haeffner <dhaeffner@gmail.com>
2022-02-23 11:10:01 -0500

    core: update error message in makeLazyLoadOptions based on feedback

--- packages/eyes-sdk-core/lib/sdk/CheckSettingsUtils.js
+++ packages/eyes-sdk-core/lib/sdk/CheckSettingsUtils.js
@@ -236,7 +236,10 @@ function makeLazyLoadOptions(input) {
   if (!input) return
   else if (input === true) return defaultOptions
   else if (TypeUtils.isObject(input)) return Object.assign({}, defaultOptions, input)
-  else throw new Error('Invalid type provided. Provide either nothing, a boolean, or an object')
+  else
+    throw new Error(
+      `Invalid type provided for the lazyLoad option. The value provided was of type ${typeof input}. Please provide either nothing, a boolean, or an object.`,
+    )
 }
 
 module.exports = {
