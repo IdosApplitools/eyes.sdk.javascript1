a4eaa76f51951443715ee7ad82368767e54a8c2a
Dave Haeffner <dhaeffner@gmail.com>
2022-04-05 11:52:43 -0400

    snippets: update isElementScrollable to also check for overflow: hidden
    Fixes screenshoter/test/e2e/web/inner-element-fully.spec.js and the following
    coverage tests:
    - CheckRegionBySelectorInFrameFullyWithCssStitching.spec.js
    - CheckRegionBySelectorInFrameFullyWithCssStitchingClassic.spec.js
    - CheckRegionBySelectorInFrameFullyWithScrollStitching.spec.js
    - CheckRegionBySelectorInFrameFullyWithScrollStitchingClassic.spec.js
    - CheckRegionBySelectorInFrameFullyOnFirefoxLegacy.spec.js
    - CheckRegionBySelectorInFrameMultipleTimesWithCssStitching.spec.js
    - CheckRegionBySelectorInFrameMultipleTimesWithScrollStitching.spec.js
    - ShouldHideAndRestoreScrollbarsWithCssStitching.spec.js
    - ShouldHideAndRestoreScrollbarsWithScrollStitching.spec.js
    - ShouldSendDomAndLocationWhenCheckRegionBySelectorFully.spec.js

--- packages/snippets/src/browser/isElementScrollable.js
+++ packages/snippets/src/browser/isElementScrollable.js
@@ -1,10 +1,11 @@
 const getElementComputedStyleProperties = require('./getElementComputedStyleProperties')
 
 function isElementScrollable([element] = []) {
-  const p = getElementComputedStyleProperties([element, ['overflow-x', 'overflow-y']])
+  const p = getElementComputedStyleProperties([element, ['overflow-x', 'overflow-y', 'overflow']])
   return (
     ((p[0] === 'scroll' || p[0] === 'auto') && (element.scrollWidth > element.clientWidth)) ||
-    ((p[1] === 'scroll' || p[1] === 'auto') && (element.scrollHeight > element.clientHeight))
+    ((p[1] === 'scroll' || p[1] === 'auto') && (element.scrollHeight > element.clientHeight)) ||
+    ((p[2] === 'hidden') && (element.scrollHeight > element.clientHeight))
   )
 }
 
